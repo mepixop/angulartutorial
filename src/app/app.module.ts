867ec811c349a71df3936d5154c987c96f95688d
Pixop <mepixop@gmail.com>
2023-09-22 19:00:03 +0200

    component lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
+/*bundles of funktionalities of the app*/
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './Server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningalertComponent,
+    SuccessAlertComponent,
   ],
   imports: [
     BrowserModule
