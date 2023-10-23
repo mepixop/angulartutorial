3662bf380847d599771fe159da24d41370a6c3ae
Pixop <mepixop@gmail.com>
2023-10-23 18:25:00 +0200

    form assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EmailValidator, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm
+  defaultSubscriptionOption = "Advanced"
+  subscriptionOptions = ["Basic", "Advanced", "Pro"]
+  submitted: boolean = false
+
+  user = {
+    email: '',
+    subscriptionOption: "",
+    password: ""
+  };
+
+  onSubmit() {
+    //PRINT values and then reset
+    this.submitted = true
+    this.user.email = this.signupForm.value.email
+    this.user.subscriptionOption = this.signupForm.value.subscriptionOptions
+    this.user.password = this.signupForm.value.password
+  }
 }
